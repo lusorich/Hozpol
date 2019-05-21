7b04b481c2e5f348206a9a34ae0c684059968664
lusorich <dreamonautre@gmail.com>
2019-05-21 15:37:17 +0500

    Доработки

---
+++ Source/sass/blocks/link.php
@@ -0,0 +1,12 @@
+.link {
+
+  &--landing {
+      margin-top: 52px;
+    background-color: #0078B3;
+    padding: 12px 25px 12px 25px;
+    color: white;
+    text-transform: uppercase;
+    border-radius: -9%;
+    border-radius: 20px;
+  }
+}
