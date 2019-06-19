1b794e186db21bcac5aecf97d11653e6df78de52
Lusorich <dreamonautre@gmail.com>
2019-06-19 23:30:21 +0500

    Добавлены скроллы и изменение стилей

--- Source/js/script.js
+++ Source/js/script.js
@@ -1,6 +1,7 @@
   let navMain = document.querySelector('.main-nav');
   let navToggle = document.querySelector('.main-nav__toggle');
   let header = document.querySelector('.page-header');
+  let menuList = document.querySelector('.product__menu-list');
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -21,3 +22,42 @@
       header.classList.remove('page-header--white');
     }
   }
+
+var element = document.querySelector('.contacts__title');
+
+var Visible = function (target) {
+  // Все позиции элемента
+  var targetPosition = {
+      top: window.pageYOffset + target.getBoundingClientRect().top,
+      left: window.pageXOffset + target.getBoundingClientRect().left,
+      right: window.pageXOffset + target.getBoundingClientRect().right,
+      bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+    },
+    // Получаем позиции окна
+    windowPosition = {
+      top: window.pageYOffset,
+      left: window.pageXOffset,
+      right: window.pageXOffset + document.documentElement.clientWidth,
+      bottom: window.pageYOffset + document.documentElement.clientHeight
+    };
+
+  if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+    menuList.classList.add('menu-list--position');
+  } else {
+    // Если элемент не видно, то запускаем этот код
+    console.clear();
+    menuList.classList.remove('menu-list--position');
+  };
+};
+
+// Запускаем функцию при прокрутке страницы
+window.addEventListener('scroll', function() {
+  Visible (element);
+});
+
+// А также запустим функцию сразу. А то вдруг, элемент изначально видно
+Visible (element);
