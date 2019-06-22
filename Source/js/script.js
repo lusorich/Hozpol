55da6dc517f502fdd04679b2f19b707113751cf4
Lusorich <dreamonautre@gmail.com>
2019-06-23 02:06:15 +0500

    Якорь

--- Source/js/script.js
+++ Source/js/script.js
@@ -2,6 +2,7 @@
   let navToggle = document.querySelector('.main-nav__toggle');
   let header = document.querySelector('.page-header');
   let menuList = document.querySelector('.product__menu-list');
+  let productItem = document.querySelectorAll('.product__menu-item');
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -14,6 +15,17 @@
       navMain.classList.remove('main-nav--opened');
     }
   });
+
+  for (let i = 0; i < productItem.length; i++) {
+      productItem[i].addEventListener('click', function() {
+        for (let i = 0; i < productItem.length; i++) {
+          if (productItem[i].classList.contains('product__menu-item--active')) {
+            productItem[i].classList.remove('product__menu-item--active');
+          }
+        }
+        productItem[i].classList.add('product__menu-item--active');
+      })
+  }
 
   window.onscroll = function() {
     if (window.pageYOffset > 100) {
